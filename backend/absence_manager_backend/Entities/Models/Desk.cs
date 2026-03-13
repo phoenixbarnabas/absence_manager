95ecdd59755a51a7d13f0c576d724ec5eaf2bfc8
batoriandrasphx <batori_andras@phoenix.hu>
2026-03-13 09:56:04 +0100

    room model added

---
+++ backend/absence_manager_backend/Entities/Models/Desk.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities.Models
+{
+    public class Desk
+    {
+        public string Id { get; set; }
+        public int RoomId { get; set; }
+        public string Name { get; set; }
+        public Room Room { get; set; }
+    }
+}
--- backend/absence_manager_backend/Entities/Models/Room.cs
+++ backend/absence_manager_backend/Entities/Models/Room.cs
@@ -2,7 +2,7 @@
 {
     public class Room
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string name { get; set; }
 
     }
