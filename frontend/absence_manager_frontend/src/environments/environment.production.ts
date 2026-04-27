60025d3575951bde9ff631fa90867839d4ca29a9
batoriandrasphx <batori_andras@phoenix.hu>
2026-04-27 13:19:00 +0200

    Refactor Dockerfile to remove entrypoint script and update environment configuration for production

---
+++ frontend/absence_manager_frontend/src/environments/environment.production.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://localhost:7190/api',
+    tenantId: '1878a48b-63d6-4d12-a900-07d4267f6762',
+    clientId: 'cacb868f-e5d8-4113-acde-780f810c824d',
+    apiScope: 'api://cacb868f-e5d8-4113-acde-780f810c824d/user_impersonation',
+    postLogoutRedirectUri: 'http://localhost:4200/welcome'
+}
