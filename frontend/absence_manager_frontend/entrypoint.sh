de938e03f4b477c28d4c4611321f23056b52c71a
batoriandrasphx <batori_andras@phoenix.hu>
2026-05-05 11:26:54 +0200

    angular build optimalization

--- frontend/absence_manager_frontend/entrypoint.sh
+++ frontend/absence_manager_frontend/entrypoint.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env sh
 set -e
 
-TEMPLATE="/usr/share/nginx/html/assets/config.example.json"
-TARGET="/usr/share/nginx/html/assets/config.json"
+TEMPLATE="/usr/share/nginx/html/config.example.json"
+TARGET="/usr/share/nginx/html/config.json"
 
 if [ -f "$TEMPLATE" ]; then
   echo "INFO: Generating runtime config: $TARGET"
-
   envsubst < "$TEMPLATE" > "$TARGET"
 else
-  echo "WARN: $TEMPLATE nem található, runtime config generálás kihagyva."
+  echo "WARN: $TEMPLATE nem található"
 fi
 
 exec "$@"
