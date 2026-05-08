7205ddd64289ca936b6e452fc672c134c7a7a235
batoriandrasphx <batori_andras@phoenix.hu>
2026-05-08 13:18:14 +0200

    migrations

---
+++ backend/absence_manager_backend/Data/Migrations/20260506091522_Initial_prod.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial_prod : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Workstations",
+                keyColumn: "Id",
+                keyValue: "ws-5",
+                column: "Code",
+                value: "Senki");
+
+            migrationBuilder.UpdateData(
+                table: "Workstations",
+                keyColumn: "Id",
+                keyValue: "ws-7",
+                column: "Code",
+                value: "Senki");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Workstations",
+                keyColumn: "Id",
+                keyValue: "ws-5",
+                column: "Code",
+                value: "F1");
+
+            migrationBuilder.UpdateData(
+                table: "Workstations",
+                keyColumn: "Id",
+                keyValue: "ws-7",
+                column: "Code",
+                value: "F2");
+        }
+    }
+}
