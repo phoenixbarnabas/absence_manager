0ae1c5f1ca8031c4b02022b40f8d4440037abb27
phoenixbarnabas <fenyvesi_barnabas@phoenix.hu>
2026-06-09 10:24:23 +0200

    add email log entity

--- backend/absence_manager_backend/absence_manager_backend/Program.cs
+++ backend/absence_manager_backend/absence_manager_backend/Program.cs
@@ -1,4 +1,6 @@
 using Data;
+using Entities.Helpers;
+using Entities.Models;
 using Logic.Helper;
 using Logic.Logic;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -26,6 +28,8 @@ builder.Services.AddScoped<IMsGraphLogic, MsGraphLogic>();
 builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
 builder.Services.AddScoped<ICurrentUserGraphSyncService, CurrentUserGraphSyncService>();
 builder.Services.AddScoped<IUserActivityLogger, UserActivityLogger>();
+builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection(EmailSettings.SectionName));
+builder.Services.AddScoped<IEmailSender, SmtpEmailSender>();
 
 var allowedOrigins = builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>();
 
